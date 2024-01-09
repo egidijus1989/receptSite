3db351f3a11b217af28753e7a7edbbf2eb5a0cc3
egidijus <145454567+egidijus1989@users.noreply.github.com>
2024-01-09 20:35:22 +0200

    headeris

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -25,59 +25,73 @@ const Navigation = () => {
     }
   }, []);
   return (
-    <Navbar expand="lg" className="">
-        <Container className="header" id="navbarSupportedContent">
-      <Navbar.Brand href="#home" className="Logo">
-        <img src={grain}></img>Recipedia
-      </Navbar.Brand>
-      <ul className="nav center">
-        <li className="nav-item">
-          <Link to="/" className="nav-link" aria-current="page">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <div className="dropdown">
-            <button
-              className="btn dropdown-toggle fw-bold"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Categories
-            </button>
-            <ul className="dropdown-menu">
-              {categories.map((category) => (
-                <CategoriesDropdawn
-                  key={category.idCategory}
-                  name={category.strCategory}
-                  link={category.strCategory}
-                />
-              ))}
-            </ul>
-          </div>
-        </li>
-        <li className="nav-item">
-          <a href="#comunity">Community</a>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link" aria-current="page">
-            About Us
-          </Link>
-        </li>
-      </ul>
-      <Nav>
-        <Nav.Link href="#search">
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </Nav.Link>
-        <Nav.Link href="#signIn">
-          <FontAwesomeIcon icon={faUser} />
-        </Nav.Link>
-      </Nav>
-    </Container>
-    </Navbar>
-
-    
+    <nav expand="lg" className="navbar navbar-expand-lg container">
+      <div className="header container-fluid ">
+        <Navbar.Brand href="#home" className="Logo">
+          <img src={grain}></img>Recipedia
+        </Navbar.Brand>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse navbaras"
+          id="navbarSupportedContent"
+        >
+          <ul className="nav center">
+            <li className="nav-item">
+              <Link to="/" className="nav-link" aria-current="page">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <div className="dropdown">
+                <button
+                  className="btn dropdown-toggle fw-bold"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Categories
+                </button>
+                <ul className="dropdown-menu">
+                  {categories.map((category) => (
+                    <CategoriesDropdawn
+                      key={category.idCategory}
+                      name={category.strCategory}
+                      link={category.strCategory}
+                    />
+                  ))}
+                </ul>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a href="#comunity">Community</a>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link" aria-current="page">
+                About Us
+              </Link>
+            </li>
+          </ul>
+          <Nav>
+            <Nav.Link href="#search">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </Nav.Link>
+            <Nav.Link href="#signIn">
+              <FontAwesomeIcon icon={faUser} />
+            </Nav.Link>
+          </Nav>
+        </div>
+      </div>
+    </nav>
   );
 };
 export default Navigation;
